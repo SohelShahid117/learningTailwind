1cf936a05594a5930b9db7091baaf13995d8fe65
SohelShahid117 <sohelshahid09@gmail.com>
2024-08-08 18:06:08 +0600

    tailwind css 1:30 min done

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,28 @@
 module.exports = {
   // content: ["./src/**/*.{html,js}"],
   content: ["*"],
+  // theme: {
+  //   extend: {},
+  // },
   theme: {
     extend: {},
+    screens: {
+      sm: "400px",
+      md: "",
+      lg: "",
+      xl: "",
+      // tablet: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      // laptop: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      // desktop: "1280px",
+      // => @media (min-width: 1280px) { ... }
+    },
+    // colors: {
+    //   green: "#2ecc71",
+    // },
   },
   plugins: [],
 };
